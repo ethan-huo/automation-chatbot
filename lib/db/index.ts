3dbcff3962cb8ef85175573c0f3fa864dd35c906
EthanHuo <huodong.work@gmail.com>
2025-06-30 12:38:28 +0800

    fix: why build error

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,5 +2,5 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 
 export * as t from './schema'
-const client = neon(process.env.POSTGRES_URL!)
+const client = neon(process.env.POSTGRES_URL)
 export const db = drizzle(client)
